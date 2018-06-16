68d2b759461be6f7bdc85fd317bb0557333cd625
league <league@wts-imac-11.attlocal.net>
2018-06-16 12:55:59 -0700

    Badghers Lyrics

---
+++ src/Badgers.java
@@ -0,0 +1,11 @@
+import javax.swing.JOptionPane;
+public class Badgers {
+public static void main(String[] args) {
+String Basic = JOptionPane.showInputDialog(null, "Say Badger");
+JOptionPane.showMessageDialog(null, " " +Basic +Basic +Basic +Basic +Basic +Basic +Basic +Basic +Basic +Basic +Basic +Basic);
+JOptionPane.showMessageDialog(null, " Mushroom");
+JOptionPane.showMessageDialog(null, "ARGH!");
+JOptionPane.showMessageDialog(null, "Snake");
+
+}
+}
